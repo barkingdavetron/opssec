d8a325a20c20ffca8e501c3a93f3df93f76278cd
mcgluedavid <mcgluedavid@gmail.com>
2025-01-30 13:04:59 +0000

    Updated script with file permissions and job completion message

--- myscript.sh
+++ myscript.sh
@@ -12,5 +12,9 @@ touch fol_1/1_1.txt fol_1/1_2.txt fol_1/1_3.txt
 # Create files in fol_2
 touch fol_2/2_1.txt fol_2/2_2.txt fol_2/2_3.txt
 
-# Show completion message
+# Set permissions
+chmod 600 fol_1/1_1.txt fol_1/1_3.txt fol_2/2_1.txt fol_2/2_3.txt  # Read & Write only for owner
+chmod 777 fol_1/1_2.txt fol_2/2_2.txt  # Read, Write, Execute for everyone
+
+# Job completion message
 echo "Job completed."
