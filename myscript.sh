f0643ceb22418eea7c1419d517b3d71b7cb1cf95
mcgluedavid <mcgluedavid@gmail.com>
2025-01-30 12:56:51 +0000

    first commit

---
+++ myscript.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+# Create two folders
+mkdir -p fol_1 fol_2
+
+# Create files in fol_1
+touch fol_1/1_1.txt fol_1/1_2.txt fol_1/1_3.txt
+
+# Create files in fol_2
+touch fol_2/2_1.txt fol_2/2_2.txt fol_2/2_3.txt
+
+echo "created folders and files."
